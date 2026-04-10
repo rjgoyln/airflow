27dab59b18961450d80342c4b394bb936ec7ce34
rjgoyln <pt40419@gmail.com>
2026-04-10 18:28:43 +0800

    seperate test case

--- helm-tests/tests/helm_tests/airflow_aux/test_database_cleanup.py
+++ helm-tests/tests/helm_tests/airflow_aux/test_database_cleanup.py
@@ -508,27 +508,28 @@ class TestDatabaseCleanupServiceAccount:
         )
         assert jmespath.search("automountServiceAccountToken", docs[0]) is False
 
+    def test_ttl_seconds_after_finished_default_behavior(self):
+        values = {"databaseCleanup": {"enabled": True}}
+        docs = render_chart(
+            values=values,
+            show_only=["templates/database-cleanup/database-cleanup-cronjob.yaml"],
+        )
+
+        assert "ttlSecondsAfterFinished" not in docs[0]["spec"]["jobTemplate"]["spec"]
+
     @pytest.mark.parametrize(
         ("ttl_value", "expected_rendered"),
         [
             (300, 300),
             (0, 0),
-            (None, None),
         ],
     )
     def test_ttl_seconds_after_finished_rendering(self, ttl_value, expected_rendered):
-        values = {"databaseCleanup": {"enabled": True}}
-        if ttl_value is not None:
-            values["databaseCleanup"]["ttlSecondsAfterFinished"] = ttl_value
-
+        values = {"databaseCleanup": {"enabled": True, "ttlSecondsAfterFinished": ttl_value}}
         docs = render_chart(
             values=values,
             show_only=["templates/database-cleanup/database-cleanup-cronjob.yaml"],
         )
 
         actual_ttl = jmespath.search("spec.jobTemplate.spec.ttlSecondsAfterFinished", docs[0])
-
-        if expected_rendered is not None:
-            assert actual_ttl == expected_rendered
-        else:
-            assert "ttlSecondsAfterFinished" not in docs[0]["spec"]["jobTemplate"]["spec"]
+        assert actual_ttl == expected_rendered
